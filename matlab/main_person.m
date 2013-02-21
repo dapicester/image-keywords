f4b46e21ea2c800a1167e60f7038ad65d65e0da1
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-02-21 17:30:26 +0800

    Added statistics (accuracy, precision, recall, fscore)

--- matlab/main_person.m
+++ matlab/main_person.m
@@ -15,9 +15,7 @@ loadData
 model = trainOneSVM(labels, histograms);
 
 % evaluate on training data
-[predictedLabels, accuracy, scores] = predictSVM(labels, histograms, model);
-fprintf('Training accuracy: %.4f%% (%d/%d)\n', ...
-        accuracy(1), numel(find(predictedLabels == labels)), length(labels)); 
+[predictedLabels, ~, scores] = predictSVM(labels, histograms, model);
 
 % visualize the ranked list of images
 % figure(1), clf, set(1, 'name', 'Ranked training images (subset)');
@@ -27,11 +25,16 @@ fprintf('Training accuracy: %.4f%% (%d/%d)\n', ...
 % figure(2), clf, set(2, 'name', 'Precision-recall on train data');
 % vl_pr(labels, scores);
 
+% results
+[~, ~, info] = vl_pr(labels, scores);
+fprintf('Train AP: %.2f\n', info.auc);
+
+fprintf(' === Training results ===\n');
+stats(predictedLabels, labels);
+
 %% Classify test images and assess performance
 
-[predictedLabels, accuracy, scores] = predictSVM(testLabels, testHistograms, model);
-fprintf('Testing accuracy: %.4f%% (%d/%d)\n', ...
-        accuracy(1), numel(find(predictedLabels == testLabels)), length(testLabels)); 
+[predictedLabels, ~, scores] = predictSVM(testLabels, testHistograms, model);
 
 % visualize the ranked list of images
 figure(3), clf, set(3, 'name', 'Ranked testing images (subset)');
@@ -44,7 +47,10 @@ vl_pr(testLabels, scores);
 % results
 [~, ~, info] = vl_pr(testLabels, scores);
 fprintf('Test AP: %.2f\n', info.auc);                                                                                                                                                                           
- 
+
+fprintf('Testing results:\n');
+stats(predictedLabels, testLabels);
+
 [~, perm] = sort(scores, 'descend');
 topK = 36;
-fprintf('Correctly retrieved in the top %d: %d\n', topK, sum(testLabels(perm(1:topK)) > 0));
+fprintf('\nCorrectly retrieved in the top %d: %d\n', topK, sum(testLabels(perm(1:topK)) > 0));
---
+++ matlab/stats.m
@@ -0,0 +1,46 @@
+function res = stats(actual, expected, varargin)
+% STATS  Compute statistics on classification results.
+%
+%   RESULTS = STATS(ACTUAL, EXPECTED) Compute statistics and returns a
+%   structure containing the fields 'accuracy', 'precision', 'recall' and
+%   'fscore'. 
+%
+%   Use STATS(..., 'plot', true) to plot results.%   
+%   Use STATS(..., 'print', true) to print statistics results.
+%
+% Author: Paolo D'Apice
+
+opts.plot = false;
+opts.print = true;
+opts = vl_argparse(opts, varargin);
+
+len = @(in) numel(find(in));
+
+pos_i = find(expected == 1);
+
+num = numel(actual);
+acc = len(actual == expected);
+
+true_pos = len(actual(pos_i) == 1);
+false_neg = len(actual(pos_i) ~= 1);
+pos = len(actual == 1);
+
+res.accuracy = acc / num;
+res.precision = true_pos / pos;
+res.recall = true_pos / (true_pos + false_neg);
+res.fscore = 2 * res.precision * res.recall / (res.precision + res.recall);
+
+if opts.print
+    % percentages
+    fprintf('  accuracy: %8.4f%%\t(%d/%d)\n', 100 * res.accuracy, acc, num);
+    fprintf(' precision: %8.4f%%\t(%d/%d)\n', 100 * res.precision, true_pos, pos);
+    fprintf('    recall: %8.4f%%\t(%d/%d)\n', 100 * res.recall, true_pos, true_pos + false_neg);
+    fprintf('   f-score: %8.4f%%\n', 100 * res.fscore);
+end
+
+if opts.plot
+    figure(99), clf
+    bar(expected, 'r'), xlim([1 num])
+    hold on
+    stem(actual)
+end
