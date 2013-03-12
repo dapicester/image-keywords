e048522ee46eddbdab04a9ad7a03cc09fa349f94
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-03-12 14:46:27 +0800

    Added more kernels
    Added two chi-squared kernels and histogram intersection kernel.

---
+++ matlab/chi2expKernel.m
@@ -0,0 +1,29 @@
+function out = chi2expKernel(x, y, varargin)
+% CHI2KERNEL  Compute the exponential chi-square kernel.
+%   OUT = CHI2KERNEL(X,Y)  Compute the exponential chi-square kernel defined as:
+%
+%      K(X,Y) = exp(-gamma * chi2(X, Y))
+%
+%   where
+%                       (x_i - y_i)^2
+%      chi2(X,Y) =  Sum -------------
+%                    i   (x_i + y_i)
+% 
+%   Use CHI2KERNEL(..., GAMMA) to override the default value of
+%   1/num_features.
+
+% Author: Paolo D'Apice
+
+if nargin == 2
+    gamma = 1/size(x,2);
+else
+    gamma = varargin{1};
+end
+
+out = zeros(size(x, 1), size(y, 1));
+for i = 1 : size(y, 1)
+    num = bsxfun(@minus, x, y(i,:));
+    den = bsxfun(@plus, x, y(i,:));
+    out(:,i) = sum(num.^2 ./ (den + eps), 2);
+end
+out = exp(-gamma * out);
---
+++ matlab/histintKernel.m
@@ -0,0 +1,16 @@
+function out = histintKernel(x, y)
+% HISTINTKERNEL  Histogram intersection kernel.
+%   OUT = HISTINTKERNEL(X, Y)  Compute the kernel defined as:
+%
+%   K(X, Y) = Sum min(X_i, Y_i) = 0.5 * Sum ( X_i + Y_i - |X_i - Y_i| )
+%              i                         i
+
+% Author: Paolo D'Apice
+
+n = size(x, 1);
+m = size(y, 1);
+out = zeros(n, m);
+for i = 1 : m
+    t = repmat(y(i,:), n, 1);
+    out(:,i) = 0.5 * sum(t + x - abs(t - x), 2);
+end
