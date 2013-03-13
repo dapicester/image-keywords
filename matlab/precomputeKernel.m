0f53c7b25ddebfdb06b20a97358f7ff9db53aed2
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-03-13 15:32:03 +0800

    Allow precomputing kernel on one data set

--- matlab/precomputeKernel.m
+++ matlab/precomputeKernel.m
@@ -1,8 +1,15 @@
-function [traink, testk] = precomputeKernel(kernel, train, test)
+function [traink, testk] = precomputeKernel(kernel, train, varargin)
 % PRECOMPUTEKERNEL  Get LIBSVM data matrices for precomputed kernel.
-%   [TRAIN2, TEST2] = PRECOMPUTEKERNEL(KERNEL, TRAIN, TEST)  Precompute the
-%   kernel matrix defined by the function handle KERNEL on TRAIN and TEST
-%   data.
+%
+%   MAPPED = PRECOMPUTEKERNEL(KERNEL, DATA)  Precompute the kernel matrix
+%   defined by the function hanle KERNEL on DATA.
+%
+%   [TRAINK, TESTK] = PRECOMPUTEKERNEL(KERNEL, TRAIN, TEST)  Precompute
+%   the kernel matrix defined by the function handle KERNEL on TRAIN and 
+%   TEST data.
 
 traink = [ (1:size(train,1))', kernel(train, train)];
-testk  = [ (1:size(test, 1))', kernel(test , train)];
+if nargin == 3
+    test = varargin{1};
+    testk  = [ (1:size(test, 1))', kernel(test , train)];
+end
