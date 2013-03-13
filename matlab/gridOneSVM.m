b7b8eed1d027f8f39e9cfbbed5ac43f003874117
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-03-13 15:32:52 +0800

    Fix and refactoring function modelEstimation
    The model estimation for the one-class SVM cannot be performed
    with cross-validation because no validation data exists.
    So an alternate method is using based on the nu parameter.

--- matlab/gridOneSVM.m
+++ matlab/gridOneSVM.m
@@ -1,34 +1,60 @@
-function params = gridOneSVM(labels, data, varargin)
+function params = gridOneSVM(labels, data, kernel, varargin)
 % GRIDONESVM  Grid search for one-class SVM parameter selection.
-%   PARAMS = GRIDONESVM(LABELS, DATA) Perform cross validation on training
-%   data. Use GRIDONESVM(..., 'n', VALUES) or GRIDONESVM(..., 'log2g', VALUES) 
-%   to specify values of nu for one-class SVM and log2(gamma) for the RBF
-%   kernel function.
+%   PARAMS = GRIDONESVM(LABELS, DATA, KERNEL)  Perform a grid search 
+%   on the parameters space using the given DATA and KERNEL. 
+%   The objective function is: min sqrt( (n-fsv)^2 + (n-fout)^2 )
+%   where fsv and fout are respectively the fraction of support vectors
+%   and the fraction of outliers.
+%
+%   Use GRIDONESVM(..., 'n', VALUES) to specify values of nu for the 
+%   one-class SVM formulation.
 % 
-%   PARAMS is a data structure containing the fields 'bestn', 'bestg' and
-%   'bestcv' containing the best nu, gamma and cross-validation values.
+%   PARAMS is a data structure containing the fields 'bestn' and
+%   'bestobj' containing respectively the best nu and objective function
+%   value.
 
 % Author: Paolo D'Apice
 
-opts.n = 0.1:0.1:0.8;
-opts.log2g = -15:2:5;
+opts.n = [ 0.01, 0.03, 0.05, 0.1, 0.15, 0.2, 0.3, 0.5, 0.8 ];
+%opts.log2g = -15:2:5;
 opts = vl_argparse(opts, varargin);
 
-% initialize to default values
-params.bestcv = svmtrain(labels, data, '-s 2 -v 5');
-params.bestn = NaN;
-params.bestg = NaN;
+numVectors = size(data, 1);
+params.bestobj = Inf;
+
+%for log2g = opts.log2g
+
+% precompute kernel
+train = precomputeKernel(kernel, data);
 
 for n = opts.n
-    for log2g = opts.log2g
-        options = ['-s 2 -v 5 -n ', num2str(n), ' -g ', num2str(2^log2g)];
-        cv = svmtrain(labels, data, options);
-        if (cv >= params.bestcv)
-            params.bestcv = cv;
+        % train model
+        options = [ '-s 2 -t 4 -q -n ', num2str(n) ]; %, ' -g ', num2str(2^log2g)];
+        model = trainOneSVM(labels, train, options);
+
+        % compute objective function
+        predicted = predictSVM(labels, train, model);
+        fsv = model.totalSV / numVectors;
+        fout = numel(find(predicted == -1)) / numVectors;
+        obj = objective(n, fsv, fout);
+        
+        % evaluate
+        if (obj < params.bestobj)
+            params.bestobj = obj;
             params.bestn = n;
-            params.bestg = 2^log2g;
+            %params.bestg = 2^log2g;
+            fprintf('*')
+        else
+            fprintf('.')
         end
-        fprintf('-> %g %g %g (best n=%g, g=%g, rate=%g)\n', ...
-                n, log2g, cv, params.bestn, params.bestg, params.bestcv);
-    end
+        %fprintf('-> %g %g %g (best n=%g, g=%g, rate=%g)\n', ...
+        %        n, log2g, cv, params.bestn, params.bestg, params.bestcv);
+        fprintf(' %.2f  %.6f\t(best n=%.2f  obj=%.6f)\n', ...
+                n, obj, params.bestn, params.bestobj);
 end
+%end
+
+
+function o = objective(n, fsv, fout)
+% OBJECTIVE  Compute the objective function.
+o = sqrt( (n-fsv)^2 + (n-fout)^2 );
--- matlab/modelEstimation.m
+++ matlab/modelEstimation.m
@@ -1,4 +1,4 @@
-function params = modelEstimation(class)
+function params = modelEstimation(class, kernel)
 % MODELESTIMATION  Model estimation for one-class SVM.
 %   PARAMS = MODELESTIMATION(CLASS) Perform grid search on one-class SVM
 %   parameters for the given class.
@@ -7,9 +7,9 @@ function params = modelEstimation(class)
 
 global SCALING
 
-fprintf('Model estimantion for class "%s"\n\n', class)
+fprintf('Model estimation for class "%s"\n\n', class)
 
-[~, train, test] = loadData(class);
+[train, test] = loadData(class);
 
 if SCALING
     fprintf('Scaling data\n')
@@ -19,28 +19,44 @@ end
 
 %% Grid search on coarse grid
 
-params = gridOneSVM(train.labels, train.histograms, 'n', 0.5);
-fprintf('Coarse grid:'), params                                 %#ok<NOPTS>
+params = gridOneSVM(train.labels, train.histograms, kernel);
+disp('Coarse grid parameters:'), disp(params)
+disp('Press a key to continue with finer grid'), pause
 
 %% Grid search on finer grid around previous best parameters
 
-neighbors = @(c,w,d) c - w*d : d : c + w*d;
+while true
+    delta = input('Delta? (0.05) ');
+    if isempty(delta), delta = 0.05; end
+    n = neighbors(params.bestn, 3, delta, 0.01);
+    %log2g = neighbors(log2(params.bestg), 3, 0.5);
+    disp('New values of n:'), disp(n)
+    cont = input('Accept values? (Y/n) ', 's');
+    if isempty(cont) || strcmpi(cont, 'y'), break, end
+end
 
-n = neighbors(params.bestn, 3, 0.02);
-log2g = neighbors(log2(params.bestg), 3, 0.5);
+params2 = gridOneSVM(train.labels, train.histograms, kernel, 'n', n);
+disp('Fine grid parameters:'), disp(params2)
 
-fprintf('Fine grid params:'), n, log2g                          %#ok<NOPTS>
-fprintf('Press a key to continue'), pause
-params_fine = gridOneSVM(train.labels, train.histograms, 'n', n, 'log2g', log2g);
-fprintf('Fine grid:'), params_fine                              %#ok<NOPTS>
+if params2.bestobj < params.bestobj
+    disp('Using fine grid parameters:')
+    n = params2.bestn;
+else
+    n = params.bestn;
+end
+fprintf(' n = %g\n', n);
+disp('Press a key to test'), pause
 
 %% Test
 
-model = svmtrain(train.labels, train.histograms, ['-s 2 -n ' num2str(params_fine.bestn) ' -g ' num2str(params_fine.bestg)]);
+[train.khistograms, test.khistograms] = precomputeKernel(kernel, train.histograms, test.histograms);
+options = [ '-s 2 -t 4 -q -n ', num2str(n) ]; %, ' -g ', num2str(2^log2g)];
+model = svmtrain(train.labels, train.khistograms, options);
+predictedLabels = svmpredict(test.labels, test.khistograms, model);
+stats(predictedLabels, test.labels, 'plot', true, 'print', true);
 
-% on training data
-svmpredict(train.labels, train.histograms, model);
 
-% on validation data
-predictedLabels = svmpredict(test.labels, test.histograms, model);
-stats(predictedLabels, test.labels, 'plot', true);
+function out = neighbors(center, width, delta, lower)
+% NEIGHBORS  Define neighbor intervals.
+out = center - width*delta : delta : center + width*delta;
+out = out(out >= lower);
