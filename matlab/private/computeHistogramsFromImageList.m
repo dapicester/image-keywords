1f8c15168ac4af8230e4045d2921db069a539139
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-04-09 11:07:46 +0800

    Revert "Removed caching (obsolete)"
    This reverts commit 0150d90866520d33a1d7f3dc058721e1d2ee0964.

--- matlab/private/computeHistogramsFromImageList.m
+++ matlab/private/computeHistogramsFromImageList.m
@@ -1,16 +1,18 @@
 function histograms = computeHistogramsFromImageList(vocabulary, names, varargin)
 % COMPUTEHISTOGRAMSFROMIMAGELIST  Compute historams for multiple images.
+%
 %   HISTOGRAMS = COMPUTEHISTOGRAMSFROMIMAGELIST(VOCABULARY, NAMES)
 %   computes the histograms of visual words for the list of image
 %   paths NAMES by applying iteratively COMPUTEHISTOGRAMFROMIMAGE().
 %
-%   COMPUTEHISTOGRAMSFROMIMAGELIST(VOCABULARY, NAMES, CACHE) caches
-%   the results to the CACHE directory.
+%   COMPUTEHISTOGRAMSFROMIMAGELIST(..., 'Cache', CACHE_DIR)  Caches
+%   the results to the CACHE_DIR directory.
 
 % Author: Andrea Vedaldi
 % Author: Paolo D'Apice
 
 conf.descriptors = 0;
+conf.cache = [];
 [conf, varargin] = vl_argparse(conf, varargin);
 
 switch conf.descriptors
@@ -20,11 +22,45 @@ switch conf.descriptors
 end
 opts = [opts, varargin];
 
+useCache = ~isempty(conf.cache);
+if useCache, fprintf('Using cache %s\n', conf.cache); end
+
 len = numel(names);
 histograms = cell(1, len);
 parfor i = 1:len
     fullPath = names{i};
+    if useCache
+        % try to retrieve from cache
+        histograms{i} = getFromCache(fullPath, conf.cache); %#ok<PFBNS>
+        if ~isempty(histograms{i})
+            continue
+        end
+    end
     fprintf('  Extracting histograms from %s (%d/%d)\n', fullPath, i, len);
     histograms{i} = computeHistogramFromImage(vocabulary, fullPath, opts{:}); %#ok<PFBNS>
+    if useCache
+        % save to cache
+        storeToCache(fullPath, conf.cache, histograms{i});
+    end
 end
 histograms = [histograms{:}];
+
+
+function histogram = getFromCache(fullPath, cache)
+[~, name] = fileparts(fullPath);
+cachePath = fullfile(cache, [name '.mat']);
+if exist(cachePath, 'file')
+    data = load(cachePath);
+    histogram = data.histogram;
+else
+    histogram = [];
+end
+
+
+function storeToCache(fullPath, cache, histogram)
+[~, name] = fileparts(fullPath);
+cachePath = fullfile(cache, [name '.mat']);
+vl_xmkdir(cache);
+data.histogram = histogram; %#ok<STRNU>
+save(cachePath, '-struct', 'data');
+
