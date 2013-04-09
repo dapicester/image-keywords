b5c386fcf6bdf353b2e164643e2409acb5793b50
Paolo D'Apice <paolo.dapice@izenesoft.com>
2013-04-09 15:35:48 +0800

    Using varargout when precomputing kernel

--- matlab/+svm/precomputeKernel.m
+++ matlab/+svm/precomputeKernel.m
@@ -1,4 +1,4 @@
-function [traink, testk] = precomputeKernel(kernel, train, varargin)
+function [traink, varargout] = precomputeKernel(kernel, train, varargin)
 % SVM.PRECOMPUTEKERNEL  Get LIBSVM data matrices for precomputed kernel.
 %
 %   MAPPED = SVM.PRECOMPUTEKERNEL(KERNEL, DATA)  Precompute the kernel 
@@ -7,9 +7,16 @@ function [traink, testk] = precomputeKernel(kernel, train, varargin)
 %   [TRAINK, TESTK] = SVM.PRECOMPUTEKERNEL(KERNEL, TRAIN, TEST) Precompute
 %   the kernel matrix defined by the function handle KERNEL on TRAIN and 
 %   TEST data.
+%
+%   [TRAINK, ...] = SVM.PRECOMPUTEKERNEL(KERNEL, TRAIN, ...) 
+%   Precompute the kernel matrix defined by the function handle KERNEL on
+%   TRAIN and other input data.
 
-traink = [ (1:size(train,1))', kernel(train, train)];
-if nargin == 3
-    test = varargin{1};
-    testk  = [ (1:size(test, 1))', kernel(test , train)];
+% Author: Paolo D'Apice
+
+mapping = @(a,b) [ (1:size(b,1))', kernel(b,a) ];
+
+traink = mapping(train, train);
+for i = 1:length(varargin)
+    varargout{i} = mapping(train, varargin{i}); %#ok<AGROW>
 end
